451beb5fabf5e867b8fe8cc162267891ba32033d
meDigital Development <medigital.net@gmail.com>
2023-02-14 13:58:50 +0700

    Add Toast script
    Add toast script

--- public/assets/js/functions.js
+++ public/assets/js/functions.js
@@ -1,3 +1,17 @@
 function randomString(){
     return (Math.random() + 1).toString(36).substring(4);
 }
+
+async function toast(icon, message) {
+    const Toast = Swal.mixin({
+        toast: true,
+        position: 'top-right',
+        showConfirmButton: false,
+        timer: 1500,
+        timerProgressBar: true
+    })
+    await Toast.fire({
+        icon: icon,
+        title: message
+    })
+}
