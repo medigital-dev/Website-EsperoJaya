eed129637a8dc27464a04b0a327f7fb10a035234
meDigital Development <medigital.net@gmail.com>
2023-02-17 09:40:21 +0700

    Update CMS-Panel: Fix API

--- app/Controllers/ApiService.php
+++ app/Controllers/ApiService.php
@@ -15,9 +15,9 @@ class ApiService extends BaseController
         if (!$model = $this->cekModel($table)) {
             return $this->failNotFound('Table Not Found, please check documentations', 400);;
         }
-        ($limit = $this->request->getGet('limit')) == null ? 0 : $limit;
+        $limit = $this->request->getGet('limit');
 
-        return $this->respond($model->orderBy('created_at', 'DESC')->findAll($limit), 200);
+        return $this->respond($model->orderBy('created_at', 'DESC')->findAll($limit == null ? 0 : $limit), 200);
     }
 
     public function cekModel($table)
