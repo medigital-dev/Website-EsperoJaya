0702d903e9ae79b8ab03635f2e8c82ed3ca09208
meDigital Development <medigital.net@gmail.com>
2023-02-20 09:28:49 +0700

    DB v1.4.1

---
+++ db_scema/patch/db-update_1.4.1.sql
@@ -0,0 +1,6 @@
+-- Database update script
+-- v1.4 to 1.4.1
+-- Created at 2023-02-20 09:28 WIB
+-- c 2023 - medigital.dev
+
+ALTER TABLE 'db_version' CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
