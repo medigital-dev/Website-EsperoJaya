b2f212fea5493e6c18435e4fd669848144dcc5b1
meDigital Development <medigital.net@gmail.com>
2023-02-20 09:00:48 +0700

    DB v1.4

---
+++ db_scema/db_1.4.sql
@@ -0,0 +1,330 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Feb 20, 2023 at 01:58 AM
+-- Server version: 8.0.30
+-- PHP Version: 8.0.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `website_esperojaya`
+--
+CREATE DATABASE IF NOT EXISTS `website_esperojaya` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+USE `website_esperojaya`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `administrator`
+--
+
+DROP TABLE IF EXISTS `administrator`;
+CREATE TABLE `administrator` (
+  `id` int NOT NULL,
+  `administrator_id` varchar(128) COLLATE utf8mb4_bin NOT NULL,
+  `name` varchar(128) COLLATE utf8mb4_bin NOT NULL,
+  `username` varchar(128) COLLATE utf8mb4_bin NOT NULL,
+  `email` varchar(128) COLLATE utf8mb4_bin NOT NULL,
+  `phone` varchar(24) COLLATE utf8mb4_bin NOT NULL,
+  `password` varchar(256) COLLATE utf8mb4_bin NOT NULL,
+  `is_active` tinyint(1) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `deleted_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `db_version`
+--
+
+DROP TABLE IF EXISTS `db_version`;
+CREATE TABLE `db_version` (
+  `id` int NOT NULL,
+  `version` varchar(64) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Truncate table before insert `db_version`
+--
+
+TRUNCATE TABLE `db_version`;
+--
+-- Dumping data for table `db_version`
+--
+
+INSERT DELAYED IGNORE INTO `db_version` (`id`, `version`, `created_at`) VALUES
+(1, '1.4', '2023-02-20 08:51:48');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `event`
+--
+
+DROP TABLE IF EXISTS `event`;
+CREATE TABLE `event` (
+  `id` int NOT NULL,
+  `event_id` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `title` varchar(128) COLLATE utf8mb4_bin NOT NULL,
+  `subtitle` mediumtext COLLATE utf8mb4_bin NOT NULL,
+  `date` date NOT NULL,
+  `time` time NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `deleted_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `feedback`
+--
+
+DROP TABLE IF EXISTS `feedback`;
+CREATE TABLE `feedback` (
+  `id` int NOT NULL,
+  `feedback_id` varchar(128) COLLATE utf8mb4_bin NOT NULL,
+  `name` varchar(128) COLLATE utf8mb4_bin NOT NULL,
+  `content` varchar(512) COLLATE utf8mb4_bin NOT NULL,
+  `is_active` tinyint(1) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `deleted_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `file`
+--
+
+DROP TABLE IF EXISTS `file`;
+CREATE TABLE `file` (
+  `id` int NOT NULL,
+  `file_id` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `filename` varchar(128) COLLATE utf8mb4_bin NOT NULL,
+  `alt` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+  `title` varchar(128) COLLATE utf8mb4_bin NOT NULL,
+  `url` varchar(128) COLLATE utf8mb4_bin NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `deleted_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `menu`
+--
+
+DROP TABLE IF EXISTS `menu`;
+CREATE TABLE `menu` (
+  `id` int NOT NULL,
+  `menu_id` varchar(128) COLLATE utf8mb4_bin NOT NULL,
+  `parent_id` int NOT NULL,
+  `url` varchar(128) COLLATE utf8mb4_bin NOT NULL,
+  `title` varchar(64) COLLATE utf8mb4_bin NOT NULL,
+  `type` varchar(64) COLLATE utf8mb4_bin NOT NULL,
+  `new_tab` tinyint(1) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `modified_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `page`
+--
+
+DROP TABLE IF EXISTS `page`;
+CREATE TABLE `page` (
+  `id` int NOT NULL,
+  `page_id` varchar(128) COLLATE utf8mb4_bin NOT NULL,
+  `title` varchar(128) COLLATE utf8mb4_bin NOT NULL,
+  `slug` varchar(128) COLLATE utf8mb4_bin NOT NULL,
+  `description` varchar(64) COLLATE utf8mb4_bin NOT NULL,
+  `content` longtext COLLATE utf8mb4_bin NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `deleted_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `post`
+--
+
+DROP TABLE IF EXISTS `post`;
+CREATE TABLE `post` (
+  `id` int NOT NULL,
+  `post_id` varchar(128) COLLATE utf8mb4_bin NOT NULL,
+  `title` varchar(256) COLLATE utf8mb4_bin NOT NULL,
+  `slug` varchar(256) COLLATE utf8mb4_bin NOT NULL,
+  `content` longtext COLLATE utf8mb4_bin NOT NULL,
+  `author` varchar(64) COLLATE utf8mb4_bin NOT NULL,
+  `status` enum('active','draft') CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `deleted_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `post_image`
+--
+
+DROP TABLE IF EXISTS `post_image`;
+CREATE TABLE `post_image` (
+  `id` int NOT NULL,
+  `file_id` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `post_id` varchar(128) COLLATE utf8mb4_bin NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `deleted_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `administrator`
+--
+ALTER TABLE `administrator`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `administrator_id` (`administrator_id`);
+
+--
+-- Indexes for table `db_version`
+--
+ALTER TABLE `db_version`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `event`
+--
+ALTER TABLE `event`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `event_id` (`event_id`);
+
+--
+-- Indexes for table `feedback`
+--
+ALTER TABLE `feedback`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `feedback_id` (`feedback_id`);
+
+--
+-- Indexes for table `file`
+--
+ALTER TABLE `file`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `file_id` (`file_id`);
+
+--
+-- Indexes for table `menu`
+--
+ALTER TABLE `menu`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `menu_id` (`menu_id`);
+
+--
+-- Indexes for table `page`
+--
+ALTER TABLE `page`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `page_id` (`page_id`);
+
+--
+-- Indexes for table `post`
+--
+ALTER TABLE `post`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `post_id` (`post_id`),
+  ADD UNIQUE KEY `slug` (`slug`);
+
+--
+-- Indexes for table `post_image`
+--
+ALTER TABLE `post_image`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `administrator`
+--
+ALTER TABLE `administrator`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `db_version`
+--
+ALTER TABLE `db_version`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `event`
+--
+ALTER TABLE `event`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `feedback`
+--
+ALTER TABLE `feedback`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `file`
+--
+ALTER TABLE `file`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `menu`
+--
+ALTER TABLE `menu`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `page`
+--
+ALTER TABLE `page`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `post`
+--
+ALTER TABLE `post`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `post_image`
+--
+ALTER TABLE `post_image`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ db_scema/patch/db-update_1.4.sql
@@ -0,0 +1,14 @@
+-- Database update script
+-- v1.3.2 to 1.4
+-- Created at 2023-02-20 08:53
+-- c 2023 - medigital.dev
+
+ALTER TABLE image RENAME TO file;
+ALTER TABLE `file` CHANGE `image_id` `file_id` VARCHAR(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL;
+ALTER TABLE `file` DROP INDEX `image_id`;
+ALTER TABLE `file` ADD UNIQUE(`file_id`);
+ALTER TABLE `file` CHANGE `alt` `alt` VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL;
+ALTER TABLE `file` ADD `url` VARCHAR(128) NOT NULL AFTER `title`;
+ALTER TABLE `post_image` CHANGE `filename` `file_id` VARCHAR(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL;
+CREATE TABLE `db_version` (`id` INT AUTO_INCREMENT , `version` VARCHAR(64) NOT NULL , `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP , PRIMARY KEY (`id`)) ENGINE = InnoDB;
+INSERT INTO `db_version` (`id`, `version`, `created_at`) VALUES (NULL, '1.4', CURRENT_TIMESTAMP);
