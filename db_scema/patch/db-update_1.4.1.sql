3e53cea477ba12c40f3b231fcf699e43494fdcec
meDigital Development <medigital.net@gmail.com>
2023-02-23 08:43:33 +0700

    DB v1.4.1

--- db_scema/patch/db-update_1.4.1.sql
+++ db_scema/patch/db-update_1.4.1.sql
@@ -4,3 +4,7 @@
 -- c 2023 - medigital.dev
 
 ALTER TABLE 'db_version' CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
+ALTER TABLE `file` ADD `type` VARCHAR(128) NULL AFTER `filename`;
+ALTER TABLE `post_image` RENAME TO `post_file`;
+
+INSERT INTO `db_version` (`id`, `version`, `created_at`) VALUES (NULL, '1.4.1', CURRENT_TIMESTAMP);
