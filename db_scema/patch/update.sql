54e2bbb2b2d1b7b8c74779985eb6681e14c91e5e
meDigital Development <medigital.net@gmail.com>
2023-02-28 13:28:51 +0700

    db v1.4.3

--- db_scema/patch/update.sql
+++ db_scema/patch/update.sql
@@ -1,6 +1,6 @@
 -- database update v1.4.2 to v1.4.3
+ALTER TABLE `post` CHANGE `status` `status` BOOLEAN NOT NULL;
 
 
-INSERT INTO `web_version` (`id`, `version`, `release_date`) VALUES (NULL, '1.0.10', '2023-02-24');
-INSERT INTO `cms_version` (`id`, `version`, `release_date`) VALUES (NULL, '1.1.0', '2023-02-24');
+INSERT INTO `cms_version` (`id`, `version`, `release_date`) VALUES (NULL, '1.2.0', '2023-02-28');
 INSERT INTO `db_version` (`id`, `version`, `release_date`) VALUES (NULL, '1.4.3', '2023-02-24');
