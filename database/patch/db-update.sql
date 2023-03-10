30604638c0c9fd6ea42d61e2ad695a3bffd5f75e
meDigital Development <medigital.net@gmail.com>
2023-03-10 10:29:44 +0700

    db v1.5.1

---
+++ database/patch/db-update.sql
@@ -0,0 +1,4 @@
+-- database update v1.5 to 1.5.1 update 2023-03-09
+INSERT INTO `db_version` (`id`, `version`, `release_date`) VALUES (NULL, '1.5.1', '2023-03-09');
+ALTER TABLE `file` CHANGE `size` `size` VARCHAR(64) NULL DEFAULT NULL;
+ALTER TABLE `file` ADD `size` INT NULL DEFAULT NULL AFTER `filename`;
